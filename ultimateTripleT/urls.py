c295b9cd01fd81719300841ded9bd1b04ddd9c83
Nicholas <152906156+Drake3Ne@users.noreply.github.com>
2026-02-05 13:50:11 -0500

    ran first migration

--- ultimateTripleT/urls.py
+++ ultimateTripleT/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('game/', include('game.urls')),
+]
+# Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='game/', permanent=True)),
 ]
